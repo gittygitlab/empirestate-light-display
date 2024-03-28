f1e919f4c256e8ad4d72872f043f887ff998cb74
Tyler Wahl <68124939+gittygitlab@users.noreply.github.com>
2024-03-28 12:10:08 -0400

    Update bootup_splashscreen.py
    Modified bootup splashscreen to only have one splashscreen and added an estimated bootup time as well author name and github url.

--- bootup_splashscreen.py
+++ bootup_splashscreen.py
@@ -16,8 +16,7 @@ font_path = "/usr/share/fonts/truetype/dejavu/DejaVuSans-Bold.ttf"
 font_size = 24
 font = ImageFont.truetype(font_path, font_size)
 
-message1 = "Hello\n\nnow booting..."
-message2 = "please hold\n\nawaiting network connection and script update"
+message1 = "Hello\n\nnow booting...\nestimated time: 45 seconds\n\n\nProject by Tyler Wahl (2024)\nhttps://github.com/gittygitlab/empirestate-light-display"
 
 # Create an image with a white background
 width, height = epd.width, epd.height
@@ -34,24 +33,6 @@ draw.multiline_text((x, y), message1, font=font, fill=0, align="center")
 
 # Display the image on the e-ink display
 logging.info("Displaying splash screen")
-epd.display(epd.getbuffer(image))
-
-# Wait for 5 seconds
-time.sleep(5)
-
-# Clear the image
-draw.rectangle((0, 0, width, height), fill=255)
-
-# Calculate text size and position for message2
-text_width, text_height = draw.textsize(message2, font=font)
-x = (width - text_width) // 2
-y = (height - text_height) // 2 - 10  # Slightly above center
-
-# Draw message2 on the image
-draw.multiline_text((x, y), message2, font=font, fill=0, align="center")
-
-# Display the updated image on the e-ink display
-logging.info("Displaying 'Please hold' message")
 epd.display(epd.getbuffer(image))
 
 # Sleep
