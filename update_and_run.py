019a3259a04eaa21a79e3e682270add4ca65b0ab
Tyler Wahl <68124939+gittygitlab@users.noreply.github.com>
2024-05-09 17:59:25 -0400

    Update update_and_run.py

--- update_and_run.py
+++ update_and_run.py
@@ -1,15 +1,10 @@
 import os
 import subprocess
-import RPi.GPIO as GPIO
-import time
 
 # Define paths
 REPO_URL = "https://github.com/gittygitlab/empirestate-light-display.git"
 LOCAL_REPO_PATH = "/home/administrator/empirestate"
 SCRIPT_PATH = "/home/administrator/empirestate/empirestate_display.py"
-FIRST_SERVICE_NAME = "bootup_splashscreen.service"
-SECOND_SERVICE_NAME = "update_and_run.service"
-GPIO_PIN = 24  # Example GPIO pin used by the first service
 
 def update_repo():
     os.chdir(LOCAL_REPO_PATH)  # Change current working directory to repository directory
@@ -21,27 +16,9 @@ def update_repo():
         # Clone the repository if it doesn't exist locally
         subprocess.run(["git", "clone", REPO_URL, LOCAL_REPO_PATH])
 
-def check_gpio_availability():
-    GPIO.setmode(GPIO.BCM)
-    gpio_function = GPIO.gpio_function(GPIO_PIN)
-    GPIO.cleanup()
-    return gpio_function == GPIO.IN
-
-def stop_first_service():
-    subprocess.run(["sudo", "systemctl", "stop", FIRST_SERVICE_NAME])
-
-def start_second_service():
-    subprocess.run(["sudo", "systemctl", "start", SECOND_SERVICE_NAME])
-
 def run_script():
     subprocess.run(["python3", SCRIPT_PATH])
 
 if __name__ == "__main__":
-    if check_gpio_availability():
-        start_second_service()
-    else:
-        stop_first_service()
-        time.sleep(1)  # Adjust the delay time as needed
-        start_second_service()
     update_repo()
     run_script()
