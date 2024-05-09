7f515a9a2502268ae2bab229c58fb24e6745ba0a
Tyler Wahl <68124939+gittygitlab@users.noreply.github.com>
2024-05-09 12:27:32 -0400

    Update update_and_run.py

--- update_and_run.py
+++ update_and_run.py
@@ -1,5 +1,4 @@
 import os
-import git
 import subprocess
 
 # Define paths
@@ -10,18 +9,11 @@ SCRIPT_PATH = "/home/administrator/empirestate/empirestate_display.py"
 def update_repo():
     os.chdir(LOCAL_REPO_PATH)  # Change current working directory to repository directory
     if os.path.exists(".git"):
-        repo = git.Repo(LOCAL_REPO_PATH)
-        try:
-            repo.remotes.origin.pull()
-        except git.exc.GitCommandError as e:
-            if "Your local changes to the following files would be overwritten by merge" in str(e):
-                # Discard local changes and overwrite
-                subprocess.run(["git", "reset", "--hard", "HEAD"])
-                repo.remotes.origin.pull()
-            else:
-                raise e
+        # Use git command-line tool to pull updates
+        subprocess.run(["git", "pull"])
     else:
-        git.Repo.clone_from(REPO_URL, LOCAL_REPO_PATH)
+        # Clone the repository if it doesn't exist locally
+        subprocess.run(["git", "clone", REPO_URL, LOCAL_REPO_PATH])
 
 def run_script():
     subprocess.run(["python3", SCRIPT_PATH])
